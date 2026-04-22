9ee50a27b61676621035f6a8951f615fd69bb6e4
Shivam Patel <70308097+Sh1vam@users.noreply.github.com>
2026-04-22 10:32:55 +0530

    Fix worker assertion to require at least 1 worker

--- train.py
+++ train.py
@@ -392,13 +392,8 @@ def setup_training_loop_kwargs(
 
     if workers is not None:
         assert isinstance(workers, int)
-        try:
-            '''if not workers >= 1:
-                pass'''
-            if workers<0:
-                raise UserError('--workers must be at least 0')
-        except:
-                pass
+        if not workers >= 1:
+                raise UserError('--workers must be at least 1') 
         args.data_loader_kwargs.num_workers = workers
         if workers == 0:
             args.data_loader_kwargs.prefetch_factor = None  # ← add this
